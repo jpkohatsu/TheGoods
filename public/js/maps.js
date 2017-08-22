f16e32a3eae462132aaf63a6dd62d7a163cdf53c
GreedForge <jafecafe@gmail.com>
2017-08-21 21:51:30 -0500

    map markers

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,198 +1,218 @@
-// $(document).ready(function() {
-//
-// $("body").on("click","#submit", function() {
-//   console.log("clicked");
-//   initMap();
-// });
-//
-//   $("body").on("click", "#submit", function() {
-//     console.log("click");
-//     initMap();
-//   });
+$(document).ready(function() {
+            //
 
-//============================================================= GOOGLE MAPS API ===============================================================
-console.log("Maps is connected");
+            // console.log(req.user);
+            // document.querySelector('#searchBar').addEventListener('keypress', function(e) {
+            //     var key = e.which || e.keyCode;
+            //     if (key === 13) { // 13 is enter
+            //         // code for enter
+            //         $.ajax({
+            //             url: "/mapMarkers",
+            //             data: {
+            //                 where: {
+            //                     itemName: {
+            //                         $regexp: $("#itemSearch").val()
+            //                     },
+            //                     availability: true,
+            //                     UserId: {
+            //                         $notRegexp: req.user.id
+            //                     }
 
-var map;
-var pos;
-var infowindow;
-var infoWindowUser;
-var interval;
-var GeoMarker;
-var austin;
-
-var initMap = function() {
-
-  var austin = {
-    lat: 30.2672,
-    lng: -97.7431
-  };
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: austin,
-    scrollwheel: false,
-    zoom: 11,
-    mapTypeId: "roadmap"
-  });
-
-  infowindow = new google.maps.InfoWindow();
-  // infoWindowUser = new google.maps.InfoWindow();
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      var marker = new google.maps.Marker({
-        map: map,
-        icon: './assets/images/blueDot.png',
-        position: pos
-      });
-
-      map.setCenter(pos);
-
-      // var geocoder = new google.maps.Geocoder();
-
-      geocodeAddress(map);
-
-    }, function() {
-      handleLocationError(true, infoWindowUser, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindowUser, map.getCenter());
-  }
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    // infoWindowUser.setPosition(pos);
-    infoWindowUser.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
-      'Error: Your browser doesn\'t support geolocation.');
-    infoWindowUser.open(map);
-    console.log("I just blocked this");
-  }
-};
-
-var destination;
+            //                 }
 
 
-// function geocodeAddress(geocoder, resultsMap) {
-//   var address = "6001 Shepherd Mountain Cove, Austin, TX";
-//   // var address = document.getElementById('address').value;
-//
-//   geocoder.geocode({
-//     'address': address
-//   }, function(results, status) {
-//     if (status === 'OK') {
-//       // resultsMap.setCenter(results[0].geometry.location);
-//       for (var i = 0; i < results.length; i++) {
-//         var marker = new google.maps.Marker({
-//           map: resultsMap,
-//           position: results[i].geometry.location
-//         });
-//       }
-//       // destination = results[0].geometry.location;
-//     } else {
-//       alert('Geocode was not successful for the following reason: ' + status);
-//     }
-//   });
-// }
+            //             },
+            //             method: "GET",
+            //         }).done(function() {
+            //             $(this).addClass("done");
+            //         });
+            //     }
+            // });
+            //============================================================= GOOGLE MAPS API ===============================================================
+            console.log("Maps is connected");
 
-var address = ["6001 Shepherd Mountain Cove, Austin, TX", "3303 N Lamar Blvd, Austin, TX 78705", "3704 Kerbey Ln, Austin, TX 78731", "4112 Medical Pkwy, Austin, TX 78756"];
+            var map;
+            var pos;
+            var infowindow;
+            var infoWindowUser;
+            var interval;
+            var GeoMarker;
+            var austin;
 
-function geocodeAddress(resultsMap) {
-  var geocoder = new google.maps.Geocoder();
+            var initMap = function() {
+
+                var austin = {
+                    lat: 30.2672,
+                    lng: -97.7431
+                };
+
+                map = new google.maps.Map(document.getElementById('map'), {
+                    center: austin,
+                    scrollwheel: false,
+                    zoom: 11,
+                    mapTypeId: "roadmap"
+                });
+
+                infowindow = new google.maps.InfoWindow();
+                // infoWindowUser = new google.maps.InfoWindow();
+
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+
+                        var marker = new google.maps.Marker({
+                            map: map,
+                            icon: './assets/images/blueDot.png',
+                            position: pos
+                        });
+
+                        map.setCenter(pos);
+
+                        // var geocoder = new google.maps.Geocoder();
+
+                        geocodeAddress(map);
+
+                    }, function() {
+                        handleLocationError(true, infoWindowUser, map.getCenter());
+                    });
+                } else {
+                    // Browser doesn't support Geolocation
+                    handleLocationError(false, infoWindowUser, map.getCenter());
+                }
+
+                function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+                    // infoWindowUser.setPosition(pos);
+                    infoWindowUser.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+                    infoWindowUser.open(map);
+                    console.log("I just blocked this");
+                }
+            };
+
+            var destination;
+
+            $.ajax({
+
+            })
+
+            // function geocodeAddress(geocoder, resultsMap) {
+            //   var address = "6001 Shepherd Mountain Cove, Austin, TX";
+            //   // var address = document.getElementById('address').value;
+            //
+            //   geocoder.geocode({
+            //     'address': address
+            //   }, function(results, status) {
+            //     if (status === 'OK') {
+            //       // resultsMap.setCenter(results[0].geometry.location);
+            //       for (var i = 0; i < results.length; i++) {
+            //         var marker = new google.maps.Marker({
+            //           map: resultsMap,
+            //           position: results[i].geometry.location
+            //         });
+            //       }
+            //       // destination = results[0].geometry.location;
+            //     } else {
+            //       alert('Geocode was not successful for the following reason: ' + status);
+            //     }
+            //   });
+            // }
+
+            var address = ["6001 Shepherd Mountain Cove, Austin, TX", "3303 N Lamar Blvd, Austin, TX 78705", "3704 Kerbey Ln, Austin, TX 78731", "4112 Medical Pkwy, Austin, TX 78756"];
+
+            function geocodeAddress(resultsMap) {
+                var geocoder = new google.maps.Geocoder();
 
 
-  for (var i = 0; i < address.length; i++) {
-    geocoder.geocode({
-      'address': address[i]
-    }, function(results, status) {
-      if (status === 'OK') {
-        var marker = new google.maps.Marker({
-          map: resultsMap,
-          position: results[0].geometry.location
+                for (var i = 0; i < address.length; i++) {
+                    geocoder.geocode({
+                        'address': address[i]
+                    }, function(results, status) {
+                        if (status === 'OK') {
+                            var marker = new google.maps.Marker({
+                                map: resultsMap,
+                                position: results[0].geometry.location
 
-        });
+                            });
 
-        google.maps.event.addListener(marker, 'click', function() {
-          var contentString = '<h1>Hello World</h1>';
-          // var contentString = '<div id="iw-container">' +
-          //                   '<div class="iw-title">' + place.name + '</div>' +
-          //                     '<div class="iw-content">' +
-          //                       '<div id="open">' + isOpen + '</div>' +
-          //                       '<div id="rating">' + "Rating: " + place.rating + '</div>' +
-          //                       '<img id="image" src="' + place.photos[0].getUrl({'maxWidth': 75, 'maxHeight': 75}) + '"><br>' +
-          //                       '<button id="directions" onclick="getDirections()">' + "Directions" + '</button>' +
-          //                     '</div>' +
-          //                   '</div>' +
-          //                 '</div>';
-          destination = results[0].geometry.location;
-          infowindow.setContent(contentString);
-          infowindow.open(map, this);
-        });
-      }
-      else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
+                            google.maps.event.addListener(marker, 'click', function() {
+                                var contentString = '<h1>Hello World</h1>';
+                                // var contentString = '<div id="iw-container">' +
+                                //                   '<div class="iw-title">' + place.name + '</div>' +
+                                //                     '<div class="iw-content">' +
+                                //                       '<div id="open">' + isOpen + '</div>' +
+                                //                       '<div id="rating">' + "Rating: " + place.rating + '</div>' +
+                                //                       '<img id="image" src="' + place.photos[0].getUrl({'maxWidth': 75, 'maxHeight': 75}) + '"><br>' +
+                                //                       '<button id="directions" onclick="getDirections()">' + "Directions" + '</button>' +
+                                //                     '</div>' +
+                                //                   '</div>' +
+                                //                 '</div>';
+                                destination = results[0].geometry.location;
+                                infowindow.setContent(contentString);
+                                infowindow.open(map, this);
+                            });
+                        } else {
+                            alert('Geocode was not successful for the following reason: ' + status);
+                        }
 
-    });
-  }
-}
-
+                    });
+                }
+            }
 
 
 
-// Function that creates the Google Maps Marker
-var id = 0;
-// var destination;
-var placeLoc;
-var isOpen;
 
-var travelMode = 'DRIVING';
+            // Function that creates the Google Maps Marker
+            var id = 0;
+            // var destination;
+            var placeLoc;
+            var isOpen;
 
-function getDirections() {
-  // $("#textDirections").show();
-  // $("#textDirections").css("width", "40%");
-  // $("#map").css("width", "60%");
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: {
-      lat: 30.2672,
-      lng: -97.7431
-    }
-  });
-  // $("#floating-panel").show();
-  directionsDisplay.setMap(map);
-  directionsDisplay.setPanel(document.getElementById('textDirections'));
-  var control = document.getElementById("floating-panel");
-  control.style.display = "block";
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
-  document.getElementById('mode').addEventListener('change', function() {
-    travelMode = $('#mode').val();
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-  });
+            var travelMode = 'DRIVING';
 
-  calculateAndDisplayRoute(directionsService, directionsDisplay);
-}
+            function getDirections() {
+                // $("#textDirections").show();
+                // $("#textDirections").css("width", "40%");
+                // $("#map").css("width", "60%");
+                var directionsService = new google.maps.DirectionsService;
+                var directionsDisplay = new google.maps.DirectionsRenderer;
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 12,
+                    center: {
+                        lat: 30.2672,
+                        lng: -97.7431
+                    }
+                });
+                // $("#floating-panel").show();
+                directionsDisplay.setMap(map);
+                directionsDisplay.setPanel(document.getElementById('textDirections'));
+                var control = document.getElementById("floating-panel");
+                control.style.display = "block";
+                map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+                document.getElementById('mode').addEventListener('change', function() {
+                    travelMode = $('#mode').val();
+                    calculateAndDisplayRoute(directionsService, directionsDisplay);
+                });
 
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-  directionsService.route({
-    origin: pos,
-    destination: destination,
-    travelMode: travelMode,
-    // travelMode: 'DRIVING',
-    provideRouteAlternatives: true
-  }, function(response, status) {
-    if (status === 'OK') {
-      directionsDisplay.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-}
-// });
+                calculateAndDisplayRoute(directionsService, directionsDisplay);
+            }
+
+            function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+                directionsService.route({
+                    origin: pos,
+                    destination: destination,
+                    travelMode: travelMode,
+                    // travelMode: 'DRIVING',
+                    provideRouteAlternatives: true
+                }, function(response, status) {
+                    if (status === 'OK') {
+                        directionsDisplay.setDirections(response);
+                    } else {
+                        window.alert('Directions request failed due to ' + status);
+                    }
+                });
+            }
+            // });
